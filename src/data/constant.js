eb4de0a23256e10eff4ef1308deed46fa753a844
niketjain1 <niketj2000@gmail.com>
2024-06-17 21:53:41 +0530

    added job role variable

--- src/data/constant.js
+++ src/data/constant.js
@@ -12,9 +12,9 @@ export const emailBody = {
       : emailDetails.uniqueFeature
   }.</p>
 <p>A little bit about me - I worked at a startup (Numans) for the past 10 months and have worked on building new features, optimizing product performance, and reducing order fulfillment time for the operations team by making a Slack alert notification system.</p>
-<p>I noticed that ${
-    emailDetails.companyName
-  } is hiring for the Backend Development role. I am excited about the opportunity to contribute at ${
+<p>I noticed that ${emailDetails.companyName} is hiring for the ${
+    emailDetails.jobRole
+  } role. I am excited about the opportunity to contribute at ${
     emailDetails.companyName
   }. I believe my skills and experiences align seamlessly with the company's goals.</p>
 <p>Would you be open to a quick chat about the position? I'd love to share more about my qualifications. I am excited to hear from you. PFA my resume for your reference.</p>
