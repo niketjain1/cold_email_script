40f877c621676f65d11fbdc3f16ae58a9abe6c92
niketjain1 <niketj2000@gmail.com>
2024-07-19 00:09:11 +0530

    Renamed the file

--- src/scripts/createDraftScript.js
+++ src/scripts/createDraftScript.js
@@ -1,54 +1,8 @@
-import { google } from "googleapis";
-import fs from "fs";
-import path from "path";
-import dotenv from "dotenv";
 import { emailDetails } from "../data/variable.js";
 import { emailBody } from "../data/constant.js";
-import generateEmail from "../utils//generateEmail.js";
-import makeBody from "../utils/makeBody.js";
+import generateEmail from "../utils/generateEmail.js";
 import generateEmailPermutations from "../utils/generateEmailPermutations.js";
-
-dotenv.config();
-
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-const createDraft = async (toEmails, subject, text, attachmentPath) => {
-  try {
-    const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
-
-    const raw = makeBody(
-      toEmails.join(","),
-      "your-email@gmail.com",
-      subject,
-      text,
-      attachmentPath
-    );
-
-    const draft = await gmail.users.drafts.create({
-      userId: "me",
-      requestBody: {
-        message: {
-          raw: raw,
-        },
-      },
-    });
-
-    return draft;
-  } catch (error) {
-    console.log("Error creating draft:", error);
-    throw error;
-  }
-};
+import createDraft from "../utils/createDraft.js";
 
 const main = async () => {
   const firstName = emailDetails.firstName;
