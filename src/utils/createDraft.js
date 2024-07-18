70bfb2376754b0263e12758ffe7c3927812706cd
niketjain1 <niketj2000@gmail.com>
2024-07-19 00:07:26 +0530

    Moved the createDraft function to utils

---
+++ src/utils/createDraft.js
@@ -0,0 +1,47 @@
+import { google } from "googleapis";
+import dotenv from "dotenv";
+import makeBody from "./makeBody.js";
+
+dotenv.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const createDraft = async (toEmails, subject, text, attachmentPath) => {
+  try {
+    const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
+
+    const raw = makeBody(
+      toEmails.join(","),
+      "your-email@gmail.com",
+      subject,
+      text,
+      attachmentPath
+    );
+
+    const draft = await gmail.users.drafts.create({
+      userId: "me",
+      requestBody: {
+        message: {
+          raw: raw,
+        },
+      },
+    });
+
+    return draft;
+  } catch (error) {
+    console.log("Error creating draft:", error);
+    throw error;
+  }
+};
+
+export default createDraft;
